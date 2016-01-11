bbeab794366ff0a95dccfaf0ac1a4db53cc4ab47
epotignano <epotignano@gmail.com>
2016-01-11 15:46:36 -0300

    Cambios en Courses.

--- src/main/webapp/ts-app/app/courses/courses.editor.controller.ts
+++ src/main/webapp/ts-app/app/courses/courses.editor.controller.ts
@@ -5,14 +5,36 @@ module Onesnap.Courses {
 	'use strict';
 
 	export class CourseEditorController {
-		static $inject: Array<string> = ['Course'];
+		static $inject: Array<string> = ['Course', 'StreamsService', '$stateParams'];
+
+		public formFields;
 		public course
-		constructor(private Course) {}
-		
+		public courseStream;
+
+		constructor(private Course, StreamsService: Onesnap.StreamsService, $stateParams: ng.ui.IStateParamsService) {
+			if ($stateParams['id']) {
+				Course.get({ id: $stateParams['id'] });
+				this.courseStream = StreamsService.getStream('api/teachers/' + $stateParams['id']);
+				this.courseStream.subscribe((notification) => {
+					this.course = notification.data;
+				})
+			}
+
+			this.formFields = [
+				{
+					key: 'name',
+					type: 'input',
+					templateOptions: {
+						type: 'text ',
+						label: 'Nombre'
+					}
+				}];
+		}
+
 		save() {
 			this.Course.save(this.course);
 		}
-		
+
 	}
 
 	angular
--- src/main/webapp/ts-app/app/courses/courses.list.controller.ts
+++ src/main/webapp/ts-app/app/courses/courses.list.controller.ts
@@ -3,11 +3,25 @@ module Onesnap.Courses {
 	'use strict';
 
 	export class CoursesListController {
-		static $inject: Array<string> = ['Course'];
-		constructor(private Course) {
-			this.Course.query({page: 0, size: 20}, function(result, headers) {
-            });
-			
+		static $inject: Array<string> = ['Course', 'StreamsService'];
+
+		public coursesList: Array<any>;
+		public coursesThread;
+		public courseConf;
+
+		constructor(public Course, private StreamsService: Onesnap.StreamsService ) {
+			Course.query({page:0, size:20});
+
+			this.coursesThread = this.StreamsService.getStream('api/courses');
+			this.coursesThread.subscribe((notification) => {
+				this.coursesList = notification.data;
+			})
+
+			this.courseConf = [{
+				key: 'name',
+				label: 'Nombre',
+				sort: true
+			}];
 		}
 
 	}
